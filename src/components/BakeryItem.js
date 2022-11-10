9adcef3d4835afa76ef20cf7c6053d7dddd99713
ecstaticpanther144 <114971803+ecstaticpanther144@users.noreply.github.com>
2022-11-09 19:34:34 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,14 @@
 // TODO: create a component that displays a single bakery item
+
+export default function BakeryItem(props){
+    const item = props.item
+    const buttonFunction = props.buttonFunction
+    return (
+        <div>
+            <div>{item.name}</div>
+            <div>{item.description}</div>
+            <div>{item.price}</div>
+            <div>{buttonFunction(item)}</div>
+        </div>
+    );
+}
